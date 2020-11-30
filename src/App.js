ce6976d274df239327ddb9f6ba869d501a515431
explor-wo <744806980@qq.com>
2020-11-30 14:07:49 +0800

    react环境搭建

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>my blog</h2>
+      <h3>技术栈：react + redux + ant design + egg.js + mongodb</h3>
+    </div>
+  );
+}
+
+export default App;
